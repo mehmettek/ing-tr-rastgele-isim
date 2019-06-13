15a3af98edfba75e73116962124012be2ba2c9ed
Mehmet Tek <mehmet_tek@windowslive.com>
2019-06-13 21:12:55 +0300

    first commit

---
+++ android/app/src/main/java/com/example/ing_tr_random_isim/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ing_tr_random_isim;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
